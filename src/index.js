d687ebb5c0e38983dafb208a9bc843bda1521bc2
rouvenWikom <r.diener@wikom.de>
2017-04-12 13:11:34 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+/**
+ * Created by rouven on 12.04.17.
+ */
+
+import React from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import {goBack} from 'react-router-redux'
+
+import Symbol from 'react-symbol'
+
+const BackLink = ({children, className, goBack}) =>
+    <button className={className} onClick={goBack}>
+        {children}
+    </button>;
+
+const mapDispatchToProps = dispatch => ({
+    goBack: () => dispatch(goBack())
+});
+
+const BackLinkContainer = connect(null, mapDispatchToProps)(BackLink);
+
+BackLinkContainer.defaultProps = {
+    children: <span><Symbol symbol="arrow-circle-left"/> zurück</span>,
+    className: 'btn btn-primary'
+};
+
+BackLinkContainer.propTypes = {
+    children: PropTypes.node,
+    className: PropTypes.string
+};
+
+export default BackLinkContainer;
